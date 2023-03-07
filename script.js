21e9d35beeb4f9a7a2145082e0e032bc87d0e211
Leo Almeida <leowoa@outlook.com>
2023-03-07 15:24:58 -0300

    refactored the code

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-var mainText = document.querySelector(".main-text");
-var leftIcon = document.querySelector(".left");
-var rigthIcon = document.querySelector(".right");
+const mainText = document.querySelector(".main-text");
+const leftIcon = document.querySelector(".left");
+const rigthIcon = document.querySelector(".right");
 
 
+const randomPlayerOne = "images/dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
+const randomPlayerTwo = "images/dice" + (Math.floor(Math.random() * 6) + 1) + ".png";
 
-var randomPlayerOne = Math.floor(Math.random() * 6) + 1;
-var randomPlayerTwo = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".left-img").setAttribute("src", randomPlayerOne);
+document.querySelector(".right-img").setAttribute("src", randomPlayerTwo);
+
+
 
 if (randomPlayerOne > randomPlayerTwo){
     mainText.innerHTML = "Player 1 Won";
@@ -16,52 +20,4 @@ if (randomPlayerOne > randomPlayerTwo){
     rigthIcon.setAttribute("style", "visibility: visible");
 } else {
     mainText.innerHTML = "Draw";
-}
-
-if (randomPlayerOne === 1){
-    document.querySelector(".left-img").setAttribute("src", "images/dice1.png");
-}
-
-if (randomPlayerOne === 2){
-    document.querySelector(".left-img").setAttribute("src", "images/dice2.png");
-}
-
-if (randomPlayerOne === 3){
-    document.querySelector(".left-img").setAttribute("src", "images/dice3.png");
-}
-
-if (randomPlayerOne === 4){
-    document.querySelector(".left-img").setAttribute("src", "images/dice4.png");
-}
-
-if (randomPlayerOne === 5){
-    document.querySelector(".left-img").setAttribute("src", "images/dice5.png");
-}
-
-if (randomPlayerOne === 6){
-    document.querySelector(".left-img").setAttribute("src", "images/dice6.png");
-}
-
-if (randomPlayerTwo === 1){
-    document.querySelector(".right-img").setAttribute("src", "images/dice1.png");
-}
-
-if (randomPlayerTwo === 2){
-    document.querySelector(".right-img").setAttribute("src", "images/dice2.png");
-}
-
-if (randomPlayerTwo === 3){
-    document.querySelector(".right-img").setAttribute("src", "images/dice3.png");
-}
-
-if (randomPlayerTwo === 4){
-    document.querySelector(".right-img").setAttribute("src", "images/dice4.png");
-}
-
-if (randomPlayerTwo === 5){
-    document.querySelector(".right-img").setAttribute("src", "images/dice5.png");
-}
-
-if (randomPlayerTwo === 6){
-    document.querySelector(".right-img").setAttribute("src", "images/dice6.png");
 }
